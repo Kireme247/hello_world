c623fd9591e8ad350b09ad911533df2439836f15
Kireme247 <53576708+Kireme247@users.noreply.github.com>
2023-06-26 15:22:23 -0400

    Update hello.py with new function

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 print("Hello world!")
 print("\t This is my first Git repository on GitHub.")
+def print_message(name='Python'):
+    print(f"Welcome to the class from {name}!)
+
+name = "Kyree"
+print_message(name)
